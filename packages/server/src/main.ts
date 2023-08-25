5cf044deccd553442cd46ca5ecec1a9d3a24845c
minji <321963741@naver.com>
2023-08-25 11:11:06 +0900

    feat: openAPI swagger added

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fantastic-blog')
+    .setDescription('The fantastic-blog API description')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
