2ebb9f2003b533c87e45d3acd4e45574e0133144
minji <321963741@naver.com>
2023-08-28 12:06:43 +0900

    feat: auth login logic added

--- packages/server/src/modules/auth/auth.service.ts
+++ packages/server/src/modules/auth/auth.service.ts
@@ -2,10 +2,13 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwt: JwtService,
   ) {}
 
   /**
@@ -42,6 +45,39 @@ export class AuthService {
    */
   async login(user: Partial<User>) {
     const { name, password } = user;
+
+    // no parameter required
+    if (!name || !password) {
+      throw new HttpException(
+        'name and password are required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const existUser = await this.userRepository.findOne({ where: { name } });
+
+    // throw error if user does not exist or password is incorrect
+    if (
+      !existUser ||
+      !(await User.comparePassword(password, existUser.password))
+    ) {
+      throw new HttpException('Invalid credentials', HttpStatus.NOT_FOUND);
+    }
+
+    // user status is locked
+    if (existUser.status === 'locked') {
+      throw new HttpException('Your account is locked', HttpStatus.BAD_REQUEST);
+    }
+
+    // create JWT payload
+    const payload = {
+      id: existUser.id,
+      name: existUser.name,
+      email: existUser.email,
+      role: existUser.role,
+    };
+    const token = await this.createJWT(payload);
+    return Object.assign(existUser, { token });
   }
   findAll() {
     return `This action returns all auth`;
@@ -57,5 +93,41 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  // create JWt
+  async createJWT(payload: Pick<User, 'email' | 'id' | 'name' | 'role'>) {
+    try {
+      // generate jwt
+      const accessToken = await this.generateAccessToken(payload);
+      const refreshToken = await this.generateRefreshToken(payload);
+      return {
+        accessToken,
+        refreshToken,
+      };
+    } catch (error) {
+      throw new HttpException(
+        'Something went wrong while generating the access token',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  // generate access token
+  async generateAccessToken(
+    payload: Pick<User, 'email' | 'id' | 'name' | 'role'>,
+  ) {
+    return await this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: process.env.JWT_ACCESS_SECRET,
+    });
+  }
+
+  // generate refresh token
+  async generateRefreshToken(payload: Pick<User, 'id'>) {
+    return await this.jwt.signAsync(payload, {
+      expiresIn: '7d',
+      secret: process.env.JWT_REFRESH_SECRET,
+    });
   }
 }
